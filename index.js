ea9b833f0ae8cae4e437cd12fd90a6d329a0c60e
John Tekdek <johntekdek@gmail.com>
2019-03-28 22:07:35 +0000

    changed greeting

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
-const express = require('express');
+const express = require("express");
 
 const app = express();
 
-app.get('/',(req,res)=>{
-    res.send({hi:'there'});
-
+app.get("/", (req, res) => {
+  res.send({ hi: "hello world" });
 });
 
 const PORT = process.env.PORT || 5000;
