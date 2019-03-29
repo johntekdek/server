98dc09cd84566a79bc9e938a58f12ed9ed1ed52c
Tekdek <john.tekdek@produban.co.uk>
2019-03-29 11:39:20 +0000

    added client credentials from Google

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
-const express = require("express");
+const express = require ('express');
+const passport = require ('passport');
+const GoogleStrategy = require ('passport-google-oauth20').Strategy;
 
-const app = express();
+const app = express ();
 
-app.get("/", (req, res) => {
-  res.send({ hi: "hello world" });
+passport.use (new GoogleStrategy ());
+
+//clientID 805258375604-au95intbjl9v0tof6sfobpkbm2q5ricu.apps.googleusercontent.com
+//clientSecret 8NgH8McPuo7Cr4EkkLl4ggxM
+app.get ('/', (req, res) => {
+  res.send ({hi: 'hello world'});
 });
 
 const PORT = process.env.PORT || 5000;
-
-app.listen(PORT);
+app.listen (PORT);
